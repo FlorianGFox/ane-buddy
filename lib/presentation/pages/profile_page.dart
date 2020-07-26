2524340b6eb432f24100c0463437ec194a39819b
Florian G <florian.grundmann@freenet.de>
2020-07-26 11:23:03 +0700

    Minor UI improvements.

--- lib/presentation/pages/profile_page.dart
+++ lib/presentation/pages/profile_page.dart
@@ -10,6 +10,9 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Profil'),
+      ),
       body: SafeArea(
         minimum: const EdgeInsets.all(16.0),
         child: ProfilePageBody(),
@@ -38,62 +41,96 @@ class ProfileForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        AneTextField(
-          labelText: 'Vorname',
-        ),
-        AneTextField(
-          labelText: 'Nachname',
-        ),
-        AneDateTimeField(
-          dateFormat: dateFormat,
-          firstDate: DateTime(1900),
-          initialDate: DateTime(1995),
-          lastDate: DateTime.now(),
-          labelText: 'Geburtstag',
-        ),
-        AneTextField(
-          labelText: 'Geburtsort/ggf. -land',
-        ),
-        CheckboxListTile(
-          value: false,
-          title: Text('Dr. med.'),
-          onChanged: (value) {},
-        ),
-        AneTextField(
-          labelText: 'Sonstige akademische Grade',
-        ),
-        CheckboxListTile(
-          title: Text('Ausländische Grade'),
-          value: false,
-          onChanged: (value) {},
-        ),
-        AneTextField(
-          labelText: 'Welche ausländische Grade',
-        ),
-        AneDateTimeField(
-          labelText: 'Ärztliche Prüfung',
-          dateFormat: dateFormat,
-          firstDate: DateTime(1900),
-          initialDate: DateTime.now(),
-          lastDate: DateTime.now(),
-        ),
-        AneDateTimeField(
-          labelText: 'Zahnärztliches Staatsexamen (nur bei MKG-Chirurgie)',
-          dateFormat: dateFormat,
-          firstDate: DateTime(1900),
-          initialDate: DateTime.now(),
-          lastDate: DateTime.now(),
-        ),
-        AneDateTimeField(
-          labelText: 'Approbation als Arzt bzw. Berufserlaubnis',
-          dateFormat: dateFormat,
-          firstDate: DateTime(1900),
-          initialDate: DateTime.now(),
-          lastDate: DateTime.now(),
-        ),
-      ],
+    final SizedBox smallDistance = SizedBox(height: 15.0);
+    final SizedBox mediumDistance = SizedBox(height: 30.0);
+    final TextStyle subTitle = Theme.of(context).textTheme.subtitle1;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Angaben zur Person',
+            style: subTitle,
+          ),
+          smallDistance,
+          AneTextField(
+            labelText: 'Vorname',
+          ),
+          smallDistance,
+          AneTextField(
+            labelText: 'Nachname',
+          ),
+          smallDistance,
+          AneDateTimeField(
+            dateFormat: dateFormat,
+            firstDate: DateTime(1900),
+            initialDate: DateTime(1995),
+            lastDate: DateTime.now(),
+            labelText: 'Geburtstag',
+          ),
+          smallDistance,
+          AneTextField(
+            labelText: 'Geburtsort/ggf. -land',
+          ),
+          mediumDistance,
+          Text(
+            'Akademische Grade',
+            style: subTitle,
+          ),
+          smallDistance,
+          CheckboxListTile(
+            value: false,
+            title: Text('Dr. med.'),
+            onChanged: (value) {},
+          ),
+          AneTextField(
+            labelText: 'Sonstige akademische Grade',
+          ),
+          smallDistance,
+          CheckboxListTile(
+            title: Text('Ausländische Grade'),
+            value: false,
+            onChanged: (value) {},
+          ),
+          AneTextField(
+            labelText: 'Welche ausländische Grade',
+          ),
+          mediumDistance,
+          Text(
+            'Angaben zu Prüfungen and Approbation',
+            style: subTitle,
+          ),
+          smallDistance,
+          AneDateTimeField(
+            labelText: 'Ärztliche Prüfung',
+            dateFormat: dateFormat,
+            firstDate: DateTime(1900),
+            initialDate: DateTime.now(),
+            lastDate: DateTime.now(),
+          ),
+          smallDistance,
+          AneDateTimeField(
+            labelText: 'Zahnärztliches Staatsexamen (nur bei MKG-Chirurgie)',
+            dateFormat: dateFormat,
+            firstDate: DateTime(1900),
+            initialDate: DateTime.now(),
+            lastDate: DateTime.now(),
+          ),
+          smallDistance,
+          AneDateTimeField(
+            labelText: 'Approbation als Arzt bzw. Berufserlaubnis',
+            dateFormat: dateFormat,
+            firstDate: DateTime(1900),
+            initialDate: DateTime.now(),
+            lastDate: DateTime.now(),
+          ),
+          mediumDistance,
+          RaisedButton(
+            onPressed: () {},
+            child: Text('Speichern'),
+          ),
+        ],
+      ),
     );
   }
 }
