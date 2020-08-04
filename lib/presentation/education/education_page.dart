5904a1999874f1ae5caea398c2bb2a8277aa960a
Florian G <florian.grundmann@freenet.de>
2020-08-04 14:25:55 +0700

    Implemented basic design for further education page.

--- lib/presentation/education/education_page.dart
+++ lib/presentation/education/education_page.dart
@@ -1,10 +1,63 @@
+import 'package:ane_buddy/domain/education/entities/further_education.dart';
+import 'package:ane_buddy/domain/education/entities/further_education_entry.dart';
 import 'package:flutter/material.dart';
 
 class EducationPage extends StatelessWidget {
-  const EducationPage({Key key}) : super(key: key);
+  final FurtherEducation education = FurtherEducation([
+    FurtherEducationEntry(
+      educator: 'Test Educator',
+      endDate: '01.01.2000',
+      startDate: '01.01.2000',
+      institution: 'Test Institution',
+      place: 'Test Place',
+      topic: 'Test topic',
+    ),
+    FurtherEducationEntry(
+      educator: 'Test Educator 2',
+      endDate: '01.01.2000',
+      startDate: '01.01.2000',
+      institution: 'Test Institution',
+      place: 'Test Place',
+      topic: 'Test topic',
+    ),
+  ]);
+
+  EducationPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weiterbildung'),
+      ),
+      body: SafeArea(
+        minimum: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: education.entries.length + 1,
+          itemBuilder: (context, index) {
+            if (index < education.entries.length) {
+              final entry = education.entries[index];
+              return ListTile(
+                title: _buildTile(context, entry),
+                subtitle: _buildSubtitle(context, entry),
+              );
+            } else {
+              return RaisedButton(
+                onPressed: () {},
+                child: Text('Weiterbildung hinzufügen'),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTile(BuildContext context, FurtherEducationEntry entry) {
+    return Text(entry.educator);
+  }
+
+  Widget _buildSubtitle(BuildContext context, FurtherEducationEntry entry) {
+    return Text(entry.startDate + ' - ' + entry.endDate);
   }
 }
